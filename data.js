0a8192c3332390744785f649cc033fe94f69dae7
joseimmanuel455-cpu <jose.immanuel455@smp.belajar.id>
2026-04-24 10:18:40 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -12,7 +12,7 @@ const databaseProyek = {
     "BARCELONA_FC2": {
         judul: "BARCELONA F",
         keahlian: ["bermian bola", "membuat gol", "menang"],
-        gambar: "https://cdn0-production-images-kly.akamaized.net/qg4OnBpTlT_DdI8Fn4L-dbvpYtk=/640x360/smart/filters:quality(75):strip_icc()/kly-media-production/medias/1098263/original/006182000_1451527205-_20151231REU_Barcelona_v_Betis_03.JPG",
+        gambar: "https://static.promediateknologi.id/crop/0x0:0x0/1200x600/webp/photo/p1/373/2024/12/07/Screenshot_2024-12-07-19-04-07-07_1c337646f29875672b5a61192b9010f9-785526347.jpg",
         latar: "Ini adalah proyek yang saya kerjakan untuk menunjukkan keahlian saya dalam bermain bola. Saya telah bermain untuk tim sekolah dan memenangkan beberapa turnamen lokal.",
         tantangan: " Tantangan terbesar dalam proyek ini adalah menghadapi tim yang lebih kuat dan menjaga konsistensi dalam performa saya. Namun, dengan latihan yang keras dan kerja sama tim, kami berhasil mengatasi rintangan tersebut.",
         pelajaran: " Dari proyek ini, saya belajar pentingnya kerja sama tim, disiplin, dan ketekunan dalam mencapai tujuan. Saya juga belajar bagaimana menghadapi tekanan dan tetap fokus pada permainan.",
