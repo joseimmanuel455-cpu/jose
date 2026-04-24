e7429c8ee6d4fe332e0c5c5f783f178171b57da0
joseimmanuel455-cpu <jose.immanuel455@smp.belajar.id>
2026-04-24 10:44:50 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -9,13 +9,13 @@ const databaseProyek = {
         link: ""
     },
 
-    "BARCELONA_FC2": {
-        judul: "BARCELONA F",
+    "PSMS MEDAN": {
+        judul: "PSMS MEDAN",
         keahlian: ["bermian bola", "membuat gol", "menang"],
         gambar: "https://static.promediateknologi.id/crop/0x0:0x0/1200x600/webp/photo/p1/373/2024/12/07/Screenshot_2024-12-07-19-04-07-07_1c337646f29875672b5a61192b9010f9-785526347.jpg",
-        latar: "Ini adalah proyek yang saya kerjakan untuk menunjukkan keahlian saya dalam bermain bola. Saya telah bermain untuk tim sekolah dan memenangkan beberapa turnamen lokal.",
-        tantangan: " Tantangan terbesar dalam proyek ini adalah menghadapi tim yang lebih kuat dan menjaga konsistensi dalam performa saya. Namun, dengan latihan yang keras dan kerja sama tim, kami berhasil mengatasi rintangan tersebut.",
-        pelajaran: " Dari proyek ini, saya belajar pentingnya kerja sama tim, disiplin, dan ketekunan dalam mencapai tujuan. Saya juga belajar bagaimana menghadapi tekanan dan tetap fokus pada permainan.",
+        latar: "Ini adalah sebuah tim sepak bola yang berasal dari Medan, Indonesia.  PSMS Medan memiliki sejarah panjang dalam dunia sepak bola Indonesia dan telah meraih berbagai prestasi di tingkat nasional. Tim ini dikenal dengan semangat juang yang tinggi dan dukungan fanatik dari para penggemarnya.",
+        tantangan: " Atmosfir yang sangat panas dan penuh semangat di stadion, serta tekanan untuk tampil baik di hadapan para penggemar yang setia. Namun, dengan kerja keras dan dukungan tim, kami berhasil mengatasi tantangan tersebut.",
+        pelajaran: "Dari tim ini saya belajar tentang pentingnya semangat juang, kerja keras, dan dukungan tim dalam mencapai kesuksesan. Saya juga belajar bagaimana menghadapi tekanan dan tetap fokus pada tujuan bersama.",
         link: ""
     },
 };
