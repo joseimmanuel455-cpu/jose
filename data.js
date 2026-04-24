a3f3ed6e3ac7b0f1683725c741644cefb5001bd9
joseimmanuel455-cpu <jose.immanuel455@smp.belajar.id>
2026-04-24 10:10:24 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -8,4 +8,16 @@ const databaseProyek = {
         pelajaran: " Dari proyek ini, saya belajar pentingnya kerja sama tim, disiplin, dan ketekunan dalam mencapai tujuan. Saya juga belajar bagaimana menghadapi tekanan dan tetap fokus pada permainan.",
         link: ""
     },
+
+    "BARCELONA_FC2": {
+        judul: "BARCELONA F",
+        keahlian: ["bermian bola", "membuat gol", "menang"],
+        gambar: "https://cdn0-production-images-kly.akamaized.net/qg4OnBpTlT_DdI8Fn4L-dbvpYtk=/640x360/smart/filters:quality(75):strip_icc()/kly-media-production/medias/1098263/original/006182000_1451527205-_20151231REU_Barcelona_v_Betis_03.JPG",
+        latar: "Ini adalah proyek yang saya kerjakan untuk menunjukkan keahlian saya dalam bermain bola. Saya telah bermain untuk tim sekolah dan memenangkan beberapa turnamen lokal.",
+        tantangan: " Tantangan terbesar dalam proyek ini adalah menghadapi tim yang lebih kuat dan menjaga konsistensi dalam performa saya. Namun, dengan latihan yang keras dan kerja sama tim, kami berhasil mengatasi rintangan tersebut.",
+        pelajaran: " Dari proyek ini, saya belajar pentingnya kerja sama tim, disiplin, dan ketekunan dalam mencapai tujuan. Saya juga belajar bagaimana menghadapi tekanan dan tetap fokus pada permainan.",
+        link: ""
+    },
 };
+
+
